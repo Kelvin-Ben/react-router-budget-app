30d56fc25b0669a1141acef5fc13a31de50295f1
Kelvin-Ben <kevoben14@gmail.com>
2023-05-11 18:07:27 +0300

    Add a button to delete the user

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import { Form, NavLink } from 'react-router-dom';
 import logomark from '../assets/logomark.svg';
+import { TrashIcon } from '@heroicons/react/24/solid'
 
 const Nav = ( { userName }) => {
   return (
@@ -13,6 +14,24 @@ const Nav = ( { userName }) => {
       <img src={logomark} alt="" height={30} />
       <span>HomeBudget</span>
       </NavLink>
+      {
+        userName && (
+          <Form
+            method='post'
+            action='/logout'
+            onSubmit={(event) => {
+              if (!confirm("Delete user and all data?")) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type='submit' className='btn btn--warning'>
+              <span>Delete User</span>
+              <TrashIcon width={20} />
+            </button>
+          </Form>
+        )
+      }
     </nav>
   )
 }
