0b779d94ba9e5fb46e87af68fa7d4721e03162d3
Kelvin-Ben <kevoben14@gmail.com>
2023-05-13 20:14:41 +0300

    Create a logout component to delete user

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,8 +1,11 @@
 import { redirect } from "react-router-dom";
 import { deleteItem } from "../helpers";
+import { toast } from "react-toastify";
 export async function logoutAction() {
   //delete the user
-  deleteItem({ key: "userName" })
+  deleteItem({ key: "userName" });
+  // show success notification
+  toast.success("You've deleted your account successfully!");
   //return redirect
-  return redirect('/')
+  return redirect("/");
 }
