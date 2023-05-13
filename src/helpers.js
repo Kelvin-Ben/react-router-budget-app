75f9897295ca3743504089e01dc3bc5f98e9e5a1
Kelvin-Ben <kevoben14@gmail.com>
2023-05-13 20:13:58 +0300

    Create helper function to delete an item

--- src/helpers.js
+++ src/helpers.js
@@ -4,6 +4,6 @@ export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key));
 };
 // delete item
-export const deleteItem = ({ key })=> {
-  return localStorage.removeItem(key)
-}
+export const deleteItem = ({ key }) => {
+  return localStorage.removeItem(key);
+};
