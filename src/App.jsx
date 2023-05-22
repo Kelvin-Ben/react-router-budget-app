7707a81d894da536b46ee8b089153c7ba08817df
Kelvin-Ben <kevoben14@gmail.com>
2023-05-22 22:06:31 +0300

    Remove commented code

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <Main />,
     loader: mainLoader,
-    errorElement: <Error />,
+    // errorElement: <Error />,
     children: [
       {
         index: true,
