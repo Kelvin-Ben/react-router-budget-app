167b63289c2712d376bc73a4f16534d1f7b888e8
Kelvin-Ben <kevoben14@gmail.com>
2023-06-01 21:34:32 +0300

    Enable loading of expense when the application loads at first

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,11 +6,13 @@ import { toast } from "react-toastify";
 import AddBudgetForm from "../components/AddBudgetForm";
 import AddExpenseForm from "../components/AddExpenseForm";
 import BudgetItem from "../components/BudgetItem";
+import Table from "../components/Table";
 
 export function dashboardLoader() {
   const userName = fetchData("userName");
   const budgets = fetchData("budgets") || [];
-  return { userName, budgets };
+  const expenses = fetchData("expenses");
+  return { userName, budgets, expenses };
 }
 export async function dashboardAction({ request }) {
   await waait()
@@ -54,7 +56,7 @@ export async function dashboardAction({ request }) {
   }
 }
 const Dashboard = () => {
-  const { userName, budgets } = useLoaderData();
+  const { userName, budgets, expenses } = useLoaderData();
   return (
     <>
       {userName ? (
@@ -79,6 +81,14 @@ const Dashboard = () => {
                       ))
                     }
                   </div>
+                  { expenses && expenses.length > 0 && (
+                    <div className="grid-md">
+                      <h2>Recent Expenses</h2>
+                      <Table expenses={expenses.sort((a, b) => 
+                        b.createdAt - a.createdAt
+                      )}/>
+                    </div>
+                  )}
                 </div>
             ) 
             : (
