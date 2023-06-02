286f6809087a6761e1cb180fa86ffb3e504ced75
Kelvin-Ben <kevoben14@gmail.com>
2023-06-02 20:45:08 +0300

    Modified helper functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,10 @@
-export const waait = () => new Promise(res => setTimeout(res, Math.random() * 2000))
+export const waait = () =>
+  new Promise((res) => setTimeout(res, Math.random() * 2000));
 // fecth color
 const generateRandomColor = () => {
   const existingBudgetLength = fetchData("budgets")?.length ?? 0;
-  return `${existingBudgetLength * 34} 65% 50%`
-}
+  return `${existingBudgetLength * 34} 65% 50%`;
+};
 // local storage function
 
 export const fetchData = (key) => {
@@ -11,35 +12,35 @@ export const fetchData = (key) => {
 };
 
 // create budget
-export const createBudget = ({
-  name, amount
-}) => {
+export const createBudget = ({ name, amount }) => {
   const newItem = {
     id: crypto.randomUUID(),
     name: name,
     createdAt: Date.now(),
     amount: +amount,
     color: generateRandomColor(),
-  }
+  };
   const existingBudgets = fetchData("budgets") ?? [];
-  return localStorage.setItem("budgets",
-  JSON.stringify([...existingBudgets, newItem]))
-}
+  return localStorage.setItem(
+    "budgets",
+    JSON.stringify([...existingBudgets, newItem])
+  );
+};
 // create expense
-export const createExpense = ({
-  name, amount, budgetId
-}) => {
+export const createExpense = ({ name, amount, budgetId }) => {
   const newItem = {
     id: crypto.randomUUID(),
     name: name,
     createdAt: Date.now(),
     amount: +amount,
-    budgetId: budgetId
-  }
+    budgetId: budgetId,
+  };
   const existingExpenses = fetchData("expenses") ?? [];
-  return localStorage.setItem("expenses",
-  JSON.stringify([...existingExpenses, newItem]))
-}
+  return localStorage.setItem(
+    "expenses",
+    JSON.stringify([...existingExpenses, newItem])
+  );
+};
 // delete item
 export const deleteItem = ({ key }) => {
   return localStorage.removeItem(key);
@@ -49,29 +50,29 @@ export const deleteItem = ({ key }) => {
 export const calculateSpentByBudget = (budgetId) => {
   const expenses = fetchData("expenses") ?? [];
   const budgetSpent = expenses.reduce((acc, expense) => {
-    // check if expense.id === budgetId I passed in 
-    if (expense.budgetId !== budgetId) return acc
-    
+    // check if expense.id === budgetId I passed in
+    if (expense.budgetId !== budgetId) return acc;
+
     // add the current amount to my total
-    return acc += expense.amount
-  }, 0)
+    return (acc += expense.amount);
+  }, 0);
   return budgetSpent;
-}
+};
 // FORMATING
 // formating Date
-export const formatDateToLocaleString = (epoch) => 
+export const formatDateToLocaleString = (epoch) =>
   new Date(epoch).toLocaleDateString();
 // formating percentage
 export const formatPercentage = (amt) => {
   return amt.toLocaleString(undefined, {
     style: "percent",
     minimumFractionDigits: 0,
-  })
-}
+  });
+};
 // Format currency
 export const formatCurrency = (amt) => {
   return amt.toLocaleString(undefined, {
     style: "currency",
-    currency: "USD"
-  })
-}
+    currency: "USD",
+  });
+};
