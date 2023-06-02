e864287942b39cea2cc734485f6fdbe00e6ead05
Kelvin-Ben <kevoben14@gmail.com>
2023-06-02 20:46:24 +0300

    Modified the table for expenses

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
-import ExpenseItem from './ExpenseItem'
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
 
 const Table = ({ expenses }) => {
   return (
-    <div className='table'>
+    <div className="table">
       <table>
         <thead>
           <tr>
-            {
-              ["Name", "Amount", "Date"].map((i, index) => (
-                <th key={index}>{i}</th>
-              ))
-            }
+            {["Name", "Amount", "Date"].map((i, index) => (
+              <th key={index}>{i}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {
-            expenses.map((expense) => (
-              <tr key={expense.id}>
-                {<ExpenseItem expense={expense} />}
-              </tr>  
-            ))
-          }
+          {expenses.map((expense) => (
+            <tr key={expense.id}>{<ExpenseItem expense={expense} />}</tr>
+          ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
