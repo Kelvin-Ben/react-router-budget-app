8a8bd720a30b98bd3bde38cee51056c9edf14dad
Kelvin-Ben <kevoben14@gmail.com>
2023-06-03 20:40:48 +0300

    Import the ExpensesPage component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { createBudget, createExpense, fetchData, waait } from "../helpers";
 import Intro from "../components/Intro";
 import { toast } from "react-toastify";
@@ -7,6 +7,7 @@ import AddBudgetForm from "../components/AddBudgetForm";
 import AddExpenseForm from "../components/AddExpenseForm";
 import BudgetItem from "../components/BudgetItem";
 import Table from "../components/Table";
+import ExpensesPage from "./ExpensesPage";
 
 export function dashboardLoader() {
   const userName = fetchData("userName");
@@ -15,11 +16,10 @@ export function dashboardLoader() {
   return { userName, budgets, expenses };
 }
 export async function dashboardAction({ request }) {
-  await waait()
+  await waait();
   const data = await request.formData();
-  const {_action, ...values} = Object.fromEntries(data);
-  if (_action === 'newUser') {
-
+  const { _action, ...values } = Object.fromEntries(data);
+  if (_action === "newUser") {
     try {
       localStorage.setItem("userName", JSON.stringify(values.userName));
       return toast.success(`Welcome, ${values.userName}`);
@@ -34,24 +34,24 @@ export async function dashboardAction({ request }) {
       createBudget({
         name: values.newBudget,
         amount: values.newBudgetAmount,
-      })
-      return toast.success("Budget Created")
+      });
+      return toast.success("Budget Created");
     } catch (e) {
-      throw new Error("There was a problem creating your budget")
+      throw new Error("There was a problem creating your budget");
     }
   }
 
   if (_action === "createExpense") {
     try {
       // create an expense
-     createExpense({
-      name: values.newExpense,
-      amount: values.newExpenseAmount,
-      budgetId: values.newExpenseBudget
-     })
-      return toast.success(`Expense ${values.newExpense} created!`)
+      createExpense({
+        name: values.newExpense,
+        amount: values.newExpenseAmount,
+        budgetId: values.newExpenseBudget,
+      });
+      return toast.success(`Expense ${values.newExpense} created!`);
     } catch (e) {
-      throw new Error("There was a problem creating your expense")
+      throw new Error("There was a problem creating your expense");
     }
   }
 }
@@ -65,40 +65,41 @@ const Dashboard = () => {
             Welcome back, <span className="accent">{userName}</span>
           </h1>
           <div className="grid-sm">
-            {
-              budgets && budgets.length > 0
-              ? (
-                <div className="grid-lg">
-                  <div className="flex-lg">
-                    <AddBudgetForm />
-                    <AddExpenseForm  budgets={budgets} />
-                  </div>
-                  <h2>Existing Budgets</h2>
-                  <div className="budgets">
-                    {
-                      budgets.map((budget) => (
-                        <BudgetItem key={budget.id} budget = {budget} />
-                      ))
-                    }
-                  </div>
-                  { expenses && expenses.length > 0 && (
-                    <div className="grid-md">
-                      <h2>Recent Expenses</h2>
-                      <Table expenses={expenses.sort((a, b) => 
-                        b.createdAt - a.createdAt
-                      )}/>
-                    </div>
-                  )}
+            {budgets && budgets.length > 0 ? (
+              <div className="grid-lg">
+                <div className="flex-lg">
+                  <AddBudgetForm />
+                  <AddExpenseForm budgets={budgets} />
                 </div>
-            ) 
-            : (
+                <h2>Existing Budgets</h2>
+                <div className="budgets">
+                  {budgets.map((budget) => (
+                    <BudgetItem key={budget.id} budget={budget} />
+                  ))}
+                </div>
+                {expenses && expenses.length > 0 && (
+                  <div className="grid-md">
+                    <h2>Recent Expenses</h2>
+                    <Table
+                      expenses={expenses
+                        .sort((a, b) => b.createdAt - a.createdAt)
+                        .slice(0, 6)}
+                    />
+                    {expenses.length > 6 && (
+                      <Link to="expenses" className="btn btn--dark">
+                        View all expenses
+                      </Link>
+                    )}
+                  </div>
+                )}
+              </div>
+            ) : (
               <div className="grid-sm">
                 <p>Personal budgeting is the secret to financial freedom.</p>
                 <p>Create a budget to get started!</p>
                 <AddBudgetForm />
               </div>
-            )
-          }
+            )}
           </div>
         </div>
       ) : (
