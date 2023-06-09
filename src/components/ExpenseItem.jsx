74889f0d7b480d77d67da639520e82c47a028304
Kelvin-Ben <kevoben14@gmail.com>
2023-06-10 01:05:06 +0300

    Show the budget in the expenseItem component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -3,7 +3,7 @@ import { formatCurrency, formatDateToLocaleString, getAllMatchingItems } from ".
 import { Link, useFetcher } from "react-router-dom";
 import { TrashIcon } from "@heroicons/react/24/solid";
 
-const ExpenseItem = ({ expense }) => {
+const ExpenseItem = ({ expense, showBudget }) => {
   const fetcher = useFetcher();
   const budget = getAllMatchingItems({
     category: "budgets",
@@ -16,14 +16,15 @@ const ExpenseItem = ({ expense }) => {
       <td>{expense.name}</td>
       <td>{formatCurrency(expense.amount)}</td>
       <td>{formatDateToLocaleString(expense.createdAt)}</td>
-      <td><Link
+      {showBudget && (
+        <td><Link
       to={`/budget/${budget.id}`}
       style={{
         "--accent": budget.color
       }}
       >
         {budget.name}
-      </Link></td>
+      </Link></td>)}
       <td>
         <fetcher.Form method="post">
           <input type="hidden" name="_action" value="deleteExpense" />
