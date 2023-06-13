2e0b9717f78761783c86d79edab10686086db67b
Kelvin-Ben <kevoben14@gmail.com>
2023-06-13 17:16:38 +0300

    Create the budget page to display the budgets

--- src/pages/BudgetPage.jsx
+++ src/pages/BudgetPage.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import { createExpense, deleteItem, getAllMatchingItems } from '../helpers'
-import { useLoaderData } from 'react-router-dom'
-import BudgetItem from '../components/BudgetItem';
-import AddExpenseForm from '../components/AddExpenseForm';
-import Table from '../components/Table';
-import { toast } from 'react-toastify';
+import React from "react";
+import { createExpense, deleteItem, getAllMatchingItems } from "../helpers";
+import { useLoaderData } from "react-router-dom";
+import BudgetItem from "../components/BudgetItem";
+import AddExpenseForm from "../components/AddExpenseForm";
+import Table from "../components/Table";
+import { toast } from "react-toastify";
 
 export async function budgetLoader({ params }) {
   const budget = await getAllMatchingItems({
     category: "budgets",
     key: "id",
     value: params.id,
-  })[0]
+  })[0];
 
   const expenses = await getAllMatchingItems({
     category: "expenses",
     key: "budgetId",
     value: params.id,
-  })
+  });
   if (!budget) {
     throw new Error("The budget you're trying to find doesn't exist");
   }
-  return {budget, expenses}
+  return { budget, expenses };
 }
 export async function budgetAction({ request }) {
   const data = await request.formData();
@@ -47,41 +47,39 @@ export async function budgetAction({ request }) {
         key: "expenses",
         id: values.expenseId,
       });
-      return toast.success('Expense deleted!');
+      return toast.success("Expense deleted!");
     } catch (e) {
       throw new Error("There was a problem deleting your expense");
     }
   }
 }
 
-  
 const BudgetPage = () => {
   const { budget, expenses } = useLoaderData();
   return (
-    <div className='grid-lg'
-    style={{
-      "--accent": budget.color
-    }}
+    <div
+      className="grid-lg"
+      style={{
+        "--accent": budget.color,
+      }}
     >
-      <h2 className='h2'>
-        <span className='accent'>{budget.name}</span> Overview
+      <h2 className="h2">
+        <span className="accent">{budget.name}</span> Overview
       </h2>
       <div className="flex-lg">
-        <BudgetItem budget={budget} />
+        <BudgetItem budget={budget} showDelete={true} />
         <AddExpenseForm budgets={[budget]} />
       </div>
-      {
-        expenses && expenses.length > 0 && (
-          <div className="grid-md">
-            <h2>
-              <span className="accent">{budget.name}</span> Expenses
-            </h2>
-            <Table expenses={expenses} showBudget={false} />
-          </div>
-        )
-      }
+      {expenses && expenses.length > 0 && (
+        <div className="grid-md">
+          <h2>
+            <span className="accent">{budget.name}</span> Expenses
+          </h2>
+          <Table expenses={expenses} showBudget={false} />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default BudgetPage
+export default BudgetPage;
